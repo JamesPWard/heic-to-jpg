bf3ccdfbf36f39dcab54148e6539d4f501956c52
James Ward <james.parker.ward@gmail.com>
2020-05-18 02:22:19 +0100

    Added server code with post req

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,23 +1,56 @@
 <template>
   <div>
-    <div class="input-group">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+  <form @submit.prevent="onSubmit" enctype="multipart/form-data">
+    
+      <div class="fields">
+        <label>Uploader</label>
+        <input type="file" ref="file" v-on:change="onSelect">
       </div>
-      <div class="custom-file">
-        <input type="file" class="custom-file-input" id="inputGroupFile01"
-          aria-describedby="inputGroupFileAddon01" multiple>
-        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+      <div class="fields">
+        <button>Submit</button>
       </div>
+  </form>
+    <div>
+      <h5>{{message}}</h5>
     </div>
   </div>
 </template>
 
 <script>
+import Axios from 'axios';
 export default {
   name: 'Uploader',
+  data(){ 
+    return {
+      file: "",
+      message: "Waiting..."
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+
+    async onSubmit(){
+      const formData = new FormData();
+      formData.append("file", this.file);
+
+      try{
+        await Axios.post('/upload', formData);
+        this.message = "Uploaded successfully!";
+      }
+      catch(err){
+        console.log(err);
+        this.message = "Something went wrong!";
+      }
+
+    },
+
+    onSelect(){
+      const file = this.$refs.file.files[0];
+      this.file = file;
+    }
+
   }
 }
 </script>

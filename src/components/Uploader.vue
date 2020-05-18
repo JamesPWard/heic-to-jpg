a10d7ed07d2b7df22a415f1e79e58d131655b90d
James Ward <james.parker.ward@gmail.com>
2020-05-18 02:02:11 +0100

    Started work on quick UI, setting up file server now

---
+++ src/components/Uploader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+      </div>
+      <div class="custom-file">
+        <input type="file" class="custom-file-input" id="inputGroupFile01"
+          aria-describedby="inputGroupFileAddon01" multiple>
+        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Uploader',
+  props: {
+    msg: String
+  }
+}
+</script>
